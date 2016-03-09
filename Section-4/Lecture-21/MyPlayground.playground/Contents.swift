0769414dcd52a356bbc5e5ea652f53308da931ec
Pavlo Cretsu <c.pavlo90@gmail.com>
2016-03-09 17:53:43 +0200

    Playground

---
+++ Section-4/Lecture-21/MyPlayground.playground/Contents.swift
@@ -0,0 +1,62 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+// Strings
+
+var str = "Hello, playground"
+
+var name = "Pasha"
+
+print("hello " + name + ".")
+
+// Integers (whole numbers)
+
+var int:Int = 26
+
+int = int * 2
+
+int = int / 3 //Bull shit
+
+var anotherInt:Int = 7
+
+print(int + anotherInt)
+
+print("The value of int is \(int)")
+
+// Doubles (numbers with decimals)
+
+var number:Double = 8.4
+
+print(number * Double(int))
+
+// Booleans (true or false)
+
+var isMale:Bool = true
+
+
+
+
+// Chalange (Me)
+
+var chInt:Int = 19
+
+var chDouble:Double = 26.89
+
+var chResult:Double = Double(chInt) * chDouble
+
+print("Chalange result is: \(chResult)")
+
+
+
+// Chalange (Rob)
+
+var a:Double = 5.76
+
+var b:Int = 8
+
+print("The pruduct of \(a) and \(b) is \(a * Double(b))")
+
+
+
+
