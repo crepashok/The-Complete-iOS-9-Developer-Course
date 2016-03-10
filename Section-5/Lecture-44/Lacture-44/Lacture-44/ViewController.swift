606520dec23cd097b1c53eff8f1857b70a64ea5f
Pavlo Cretsu <c.pavlo90@gmail.com>
2016-03-10 15:25:28 +0200

    Lecture-44
    Ready Timer application

--- Section-5/Lecture-44/Lacture-44/Lacture-44/ViewController.swift
+++ Section-5/Lecture-44/Lacture-44/Lacture-44/ViewController.swift
@@ -9,17 +9,81 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    var time = 0
+    
+    var timer = NSTimer()
+    
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    @IBOutlet weak var playButton: UIBarButtonItem!
+    
+    func increaseTimer() {
+    
+        resultLabel.text = "\(++time) sec."
+        
     }
+    
+    func resetTimer() {
+    
+        time = 0;
+        
+        resultLabel.text = "0 sec."
+        
+        playButton.tag = 0
+        
+        applyPlayButtonStyle()
+        
+    }
+    
+    func applyPlayButtonStyle() {
+        
+        playButton.tintColor = (playButton.tag == 0) ? UIColor.init(red: 13.0 / 255.0, green: 95.0 / 255.0, blue: 255.0 / 255.0, alpha: 1.0) : UIColor.grayColor()
+        
+    }
+    
+    
+    @IBAction func startTimerButtonClick(sender: UIBarButtonItem) {
+        
+        if sender.tag == 0 {
+            
+            timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: Selector("increaseTimer"), userInfo: nil, repeats: true)
+            
+        } else {
+            
+            timer.invalidate()
+            
+        }
+        
+        sender.tag = (sender.tag == 1) ? 0 : 1
+        
+        applyPlayButtonStyle()
+        
+    }
+
+    
+    @IBAction func pauseTimerButtonClick(sender: UIBarButtonItem) {
+        
+        timer.invalidate()
+        
+        resetTimer()
+        
+    }
+    
+    
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        
+    }
+    
 
     override func didReceiveMemoryWarning() {
+        
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
     }
-
 
 }
 
