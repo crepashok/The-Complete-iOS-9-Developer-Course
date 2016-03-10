0acdd427166ca33263ef65c27c6ee6ed4b21ed16
Pavlo Cretsu <c.pavlo90@gmail.com>
2016-03-10 12:05:29 +0200

    Lecture-35

--- Section-4/Lecture-35/Lecture-35.playground/Contents.swift
+++ Section-4/Lecture-35/Lecture-35.playground/Contents.swift
@@ -2,4 +2,31 @@
 
 import UIKit
 
+var number = 99
+
+var isPrime = true
+
+if number == 1 {
+    
+    isPrime = false
+    
+}
+
+if number != 2 && number != 1 {
+    
+    for var i = 2; i < number; i++ {
+        
+        if number % i == 0 {
+            
+            isPrime = false
+            
+            break;
+            
+        }
+        
+    }
+
+}
+
+print(isPrime)
 
