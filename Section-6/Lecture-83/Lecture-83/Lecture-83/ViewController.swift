3575235c13c3d8bd01ba625219c7946d85cbab33
Pavlo Cretsu <c.pavlo90@gmail.com>
2016-03-14 10:51:14 +0200

    Lecture-83
    Advanced Segues - Executing Code Before & After Segues (Xcode files)

---
+++ Section-6/Lecture-83/Lecture-83/Lecture-83/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  Lecture-83
+//
+//  Created by Pavlo Cretsu on 3/14/16.
+//  Copyright © 2016 Pasha Cretsu. All rights reserved.
+//
+
+import UIKit
+
+
+var rowCounter:Int = 0
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        
+        print(rowCounter)
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        
+        super.didReceiveMemoryWarning()
+        
+    }
+
+
+}
+
