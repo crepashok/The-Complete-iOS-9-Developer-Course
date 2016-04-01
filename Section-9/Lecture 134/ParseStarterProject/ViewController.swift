3e4589a430beea59474be4fad979c790b42d3f7d
Pavlo Cretsu <c.pavlo90@gmail.com>
2016-04-01 17:25:05 +0300

    Lecture-134
    Empty project

---
+++ Section-9/Lecture 134/ParseStarterProject/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//
+//  Copyright 2011-present Parse Inc. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        let testObject = PFObject(className: "TestObject")
+        testObject["foo"] = "bar"
+        testObject.saveInBackgroundWithBlock { (success, error) -> Void in
+            print("Object has been saved.")
+        }
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
