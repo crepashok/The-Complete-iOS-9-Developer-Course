828322f8635668590f438623bebda8b068031012
Pavlo Cretsu <c.pavlo90@gmail.com>
2016-04-06 11:20:53 +0300

    Lecture-195
    Implementing Version Control (Xcode files)

---
+++ Section-15/Lecture-195/Lecture-195/Lecture-195/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Lecture-195
+//
+//  Created by Pavlo Cretsu on 4/6/16.
+//  Copyright © 2016 Pasha Cretsu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBAction func buttonTapped(sender: AnyObject) {
+        
+        print("Button tapped!")
+        
+    }
+    
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        
+        print("Hello world")
+    }
+    
+    
+}
+
