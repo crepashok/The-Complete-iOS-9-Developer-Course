8fa443ea5e17657705730828f586ab8fed5906d3
Pavlo Cretsu <c.pavlo90@gmail.com>
2016-04-07 18:00:57 +0300

    Lecture-155
    Adding And Animating Sprites (Xcode files)

--- Section-10/Lecture-153/Lecture-153/GameScene.swift
+++ Section-10/Lecture-153/Lecture-153/GameScene.swift
@@ -11,9 +11,26 @@ import SpriteKit
 class GameScene: SKScene {
     
     
+    var bird = SKSpriteNode()
+    
+    
     override func didMoveToView(view: SKView) {
         
+        let birdTexture1 = SKTexture(imageNamed: "flappy1.png")
         
+        let birdTexture2 = SKTexture(imageNamed: "flappy2.png")
+        
+        let animation = SKAction.animateWithTextures([birdTexture1, birdTexture2], timePerFrame: 0.1)
+        
+        let makeBirdFlap = SKAction.repeatActionForever(animation)
+        
+        bird = SKSpriteNode(texture: birdTexture1)
+        
+        bird.runAction(makeBirdFlap)
+        
+        bird.position = CGPoint(x: CGRectGetMidX(self.frame), y: CGRectGetMidY(self.frame))
+        
+        self.addChild(bird)
         
     }
     
