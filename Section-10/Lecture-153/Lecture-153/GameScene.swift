9e9ef79b08dc324b8f3e43f734b283dd6ef62750
Pavlo Cretsu <c.pavlo90@gmail.com>
2016-04-07 18:24:20 +0300

    Lecture-157
    Animating The Background (Xcode files)

--- Section-10/Lecture-153/Lecture-153/GameScene.swift
+++ Section-10/Lecture-153/Lecture-153/GameScene.swift
@@ -13,9 +13,39 @@ class GameScene: SKScene {
     
     var bird = SKSpriteNode()
     
+    var bg = SKSpriteNode()
+    
     
     override func didMoveToView(view: SKView) {
         
+        // Adding animated background
+        let bgTexture = SKTexture(imageNamed: "bg.png")
+        
+        let movebg = SKAction.moveByX(-bgTexture.size().width, y: 0, duration: 9)
+        
+        let replaceBG = SKAction.moveByX(bgTexture.size().width, y: 0, duration: 0)
+        
+        let moveBgForever = SKAction.repeatActionForever(SKAction.sequence([movebg, replaceBG]))
+        
+        for i in 0 ..< 3 {
+            
+            let counter:CGFloat = CGFloat(i)
+            
+            bg = SKSpriteNode(texture: bgTexture)
+            
+            bg.position = CGPoint(x: bgTexture.size().width / 2 + bgTexture.size().width * counter, y: CGRectGetMidY(self.frame))
+            
+            bg.size.height = self.frame.height
+            
+            bg.runAction(moveBgForever)
+            
+            self.addChild(bg)
+            
+        }
+        
+        
+        
+        // Adding animated bird
         let birdTexture1 = SKTexture(imageNamed: "flappy1.png")
         
         let birdTexture2 = SKTexture(imageNamed: "flappy2.png")
@@ -31,6 +61,7 @@ class GameScene: SKScene {
         bird.position = CGPoint(x: CGRectGetMidX(self.frame), y: CGRectGetMidY(self.frame))
         
         self.addChild(bird)
+        
         
     }
     
