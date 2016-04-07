9409f6633c3e3ddf0079c2a6a1cc36c094f2f037
Pavlo Cretsu <c.pavlo90@gmail.com>
2016-04-07 18:41:53 +0300

    Lecture-159
    Controlling Sprites (Xcode files)

--- Section-10/Lecture-153/Lecture-153/GameScene.swift
+++ Section-10/Lecture-153/Lecture-153/GameScene.swift
@@ -56,6 +56,10 @@ class GameScene: SKScene {
         
         bird = SKSpriteNode(texture: birdTexture1)
         
+        bird.physicsBody = SKPhysicsBody(circleOfRadius: birdTexture1.size().height / 2)
+        
+        bird.physicsBody!.dynamic = true
+        
         bird.runAction(makeBirdFlap)
         
         bird.position = CGPoint(x: CGRectGetMidX(self.frame), y: CGRectGetMidY(self.frame))
@@ -63,13 +67,26 @@ class GameScene: SKScene {
         self.addChild(bird)
         
         
+        
+        // Adding ground
+        let ground = SKNode()
+        
+        ground.position = CGPointMake(0, 0)
+        
+        ground.physicsBody = SKPhysicsBody(rectangleOfSize: CGSizeMake(self.frame.size.width, 1))
+        
+        ground.physicsBody!.dynamic = false
+        
+        self.addChild(ground)
     }
     
     
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
         
+        bird.physicsBody!.velocity = CGVectorMake(0, 0)
         
+        bird.physicsBody!.applyImpulse(CGVectorMake(0, 50))
         
     }
    
